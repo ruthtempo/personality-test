970c5f7db90a2e235e00264cde9132e5aade166d
Ruth Temporal <ruthtemp@gmail.com>
2023-01-25 18:54:52 +0100

    app component wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Form } from "./pages/Form";
+import { HomePage } from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  const [currentPage, setCurrentPage] = useState("homepage");
+
+  return (
+    <ChakraProvider>
+      <Container display="flex" h="100vh" centerContent justifyContent="center">
+        <Flex>
+          {currentPage === "homepage" ? (
+            <HomePage setCurrentPage={setCurrentPage} />
+          ) : (
+            <Form />
+          )}
+        </Flex>
+      </Container>
+    </ChakraProvider>
+  );
 }
 
 export default App;
