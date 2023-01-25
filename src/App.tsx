b398882de788de70a7b99427014be9d6c3507713
Ruth Temporal <ruthtemp@gmail.com>
2023-01-25 20:24:03 +0100

    getting and setting localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
 import { useState } from "react";
+import { Answer } from "./DATA";
 import { Form } from "./pages/Form";
 import { HomePage } from "./pages/Home";
 
+const getSavedAnswers = () => {
+  const maybeSavedAnswers = localStorage.getItem("MyAnswers");
+  const savedAnswers: Answer["value"][] =
+    maybeSavedAnswers !== null ? JSON.parse(maybeSavedAnswers) : [];
+
+  return savedAnswers;
+};
+
 function App() {
   const [currentPage, setCurrentPage] = useState("homepage");
+  const [answers, setAnswers] = useState<Answer["value"][]>(getSavedAnswers());
+
+  const answerQuestion = (answer: Answer["value"]) => {
+    const newAnswers = answers.concat(answer);
+    setAnswers(newAnswers);
+    localStorage.setItem("MyAnswers", JSON.stringify(answers));
+  };
 
   return (
     <ChakraProvider>
@@ -13,7 +29,7 @@ function App() {
           {currentPage === "homepage" ? (
             <HomePage setCurrentPage={setCurrentPage} />
           ) : (
-            <Form />
+            <Form answerQuestion={answerQuestion} />
           )}
         </Flex>
       </Container>
