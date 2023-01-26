c2961b32ad4e547fdac8417b0db530abe71c196e
Ruth Temporal <ruthtemp@gmail.com>
2023-01-26 18:04:34 +0100

    reset test function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import { Answer } from "./DATA";
 import { Form } from "./pages/Form";
 import { HomePage } from "./pages/Home";
@@ -13,25 +14,35 @@ const getSavedAnswers = () => {
 };
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("homepage");
   const [answers, setAnswers] = useState<Answer["value"][]>(getSavedAnswers());
 
   const answerQuestion = (answer: Answer["value"]) => {
     const newAnswers = answers.concat(answer);
     setAnswers(newAnswers);
-    localStorage.setItem("MyAnswers", JSON.stringify(answers));
+    localStorage.setItem("MyAnswers", JSON.stringify(newAnswers));
+  };
+
+  const resetTest = () => {
+    setAnswers([]);
+    localStorage.setItem("MyAnswers", JSON.stringify([]));
   };
 
   return (
     <ChakraProvider>
-      <Container display="flex" h="100vh" centerContent justifyContent="center">
-        <Flex>
-          {currentPage === "homepage" ? (
-            <HomePage setCurrentPage={setCurrentPage} />
-          ) : (
-            <Form answerQuestion={answerQuestion} />
-          )}
-        </Flex>
+      <Container h="100vh" centerContent justifyContent="center">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/form"
+            element={
+              <Form
+                answerQuestion={answerQuestion}
+                answers={answers}
+                resetTest={resetTest}
+              />
+            }
+          />
+        </Routes>
       </Container>
     </ChakraProvider>
   );
