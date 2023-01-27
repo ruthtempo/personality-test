2a827a11000b56af09bae9e1417b2ecb34022444
Ruth Temporal <ruthtemp@gmail.com>
2023-01-27 23:00:00 +0100

    localstorage data into object

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,40 @@ import { Answer } from "./DATA";
 import { Form } from "./pages/Form";
 import { HomePage } from "./pages/Home";
 
+export interface LocalStorage {
+  answers: Answer["value"][];
+  user: string;
+}
+
 const getSavedAnswers = () => {
   const maybeSavedAnswers = localStorage.getItem("MyAnswers");
-  const savedAnswers: Answer["value"][] =
-    maybeSavedAnswers !== null ? JSON.parse(maybeSavedAnswers) : [];
+  const savedAnswers: LocalStorage =
+    maybeSavedAnswers !== null ? JSON.parse(maybeSavedAnswers) : {};
 
   return savedAnswers;
 };
 
 function App() {
-  const [answers, setAnswers] = useState<Answer["value"][]>(getSavedAnswers());
-  const [userName, setUserName] = useState<string>("");
+  const [answers, setAnswers] = useState<Answer["value"][]>(
+    getSavedAnswers().answers ?? []
+  );
+  const [userName, setUserName] = useState<string>(
+    getSavedAnswers().user ?? ""
+  );
 
   const answerQuestion = (answer: Answer["value"]) => {
     const newAnswers = answers.concat(answer);
     setAnswers(newAnswers);
-    localStorage.setItem("MyAnswers", JSON.stringify(newAnswers));
+    localStorage.setItem(
+      "MyAnswers",
+      JSON.stringify({ answers: newAnswers, user: userName })
+    );
   };
 
   const resetTest = () => {
     setAnswers([]);
-    localStorage.setItem("MyAnswers", JSON.stringify([]));
+    setUserName("");
+    localStorage.clear();
   };
 
   return (
