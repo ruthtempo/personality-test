cb6c36d8705dc79894100b34e28d0ab213d197bd
Ruth Temporal <ruthtemp@gmail.com>
2023-01-27 21:14:36 +0100

    adds tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,13 +5,31 @@ describe("Home", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("contains a card, a title and a card body with the test instructions", () => {
+  it("contains a start button,  card, a title and a card body with the test instructions", () => {
     cy.getByData("start-test-button").should("exist");
     cy.getByData("card-body").should("exist");
     cy.get("h2").should("exist");
   });
-  it("start button exists and it leads you to the form route", () => {
-    cy.getByData("start-test-button").should("exist").click();
+  it("navigates the user to the form when start button is clicked and the name field is not empty", () => {
+    cy.getByData("name-input").type("Ruth");
+    cy.getByData("start-test-button").click();
     cy.location("pathname").should("eq", "/form");
+  });
+
+  it("shows an error message if start button is clicked without name input", () => {
+    cy.getByData("name-input").should("have.value", "");
+    cy.getByData("start-test-button").click();
+    cy.getByData("error-no-name").should("exist");
+  });
+
+  it("when start button is clicked, test progress is resetted", () => {
+    cy.getByData("start-test-button")
+      .click()
+      .then(() => {
+        const localStorage: string[] = JSON.parse(
+          window.localStorage.getItem("MyAnswers") ?? "[]"
+        );
+        expect(localStorage.length).eq(0);
+      });
   });
 });
